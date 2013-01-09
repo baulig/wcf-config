b6fd6a39d09161f0f79a1a5931a45632acb7afc9
Martin Baulig <martin.baulig@xamarin.com>
2013-01-09 15:17:43 +0100

    .

--- WCF.Config/CustomBindingModule.cs
+++ WCF.Config/CustomBindingModule.cs
@@ -1,10 +1,10 @@
 //
-// BindingModule.cs
+// CustomBindingModule.cs
 //
 // Author:
 //       Martin Baulig <martin.baulig@xamarin.com>
 //
-// Copyright (c) 2012 Xamarin Inc. (http://www.xamarin.com)
+// Copyright (c) 2013 Xamarin Inc. (http://www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,25 +24,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-using System.Collections.Generic;
-using System.Xml;
-using System.Xml.Schema;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 
 namespace WCF.Config {
 
-	public class BindingsModule : CollectionModule<Binding> {
+	public class CustomBindingModule : BindingModule<CustomBinding> {
 
 		public override string Name {
-			get { return "bindings"; }
-		}
-
-		protected override void Populate ()
-		{
-			AddElement<BasicHttpBinding, BasicHttpBindingModule> ();
-			AddElement<CustomBinding, CustomBindingModule> ();
-			base.Populate ();
+			get { return "customBinding"; }
 		}
 
 	}
